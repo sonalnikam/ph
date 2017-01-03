6b1c98b456fbbd9c146715e8fc548c3470e93a57
sonalnikam <sonal.nikam@atos.net>
2017-01-03 09:32:23 +0530

    v1

--- app/webroot/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', '172.30.11.121:3306');
-   define('DB_USERNAME', 'sonal');
-     define('DB_PASSWORD', 'sonal');
-   define('DB_DATABASE','mydb');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) ;
-?>
